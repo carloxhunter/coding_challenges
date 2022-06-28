0f6d6415906421e14ed4dcd26ef864470c57849e
elguet5 <carloxhunter@gmail.com>
2022-06-27 20:13:21 -0400

    se anaden method seleccionar ith en lista simple

--- sorting/list.py
+++ sorting/list.py
@@ -48,6 +48,7 @@ class DoubleNode:
 class SimpleLinkedList:
     def __init__(self):
         self.head = None #actually the first element
+        self.elements = 0
     def printList(self):
         print('SimpleLinkedList')
         node = self.head
@@ -57,6 +58,7 @@ class SimpleLinkedList:
                 node.printself()
                 node = node.next
     def push(self, data):
+        self.elements+=1
         node=self.head
         if not node: self.head=SimpleNode(data)
         else:
@@ -65,6 +67,19 @@ class SimpleLinkedList:
             #this should be the last node since the previous loop didnt go in coz
             #node.next is None
             node.next=SimpleNode(data)
+    def select_ith(self,ith):
+        if not self.head: return None
+        elif ith >= self.elements or ith < 0: return None
+        else:
+            C=0
+            node=self.head
+            while node.next and C!= ith:
+                node=node.next
+                C+=1
+            return node
+    
+                
+
             
 
 class DoubleLinkedList:
@@ -116,5 +131,11 @@ DoubleL2.push(6)
 DoubleL2.push(5)
 DoubleL2.push(4)
 DoubleL2.pushTop(11)
-DoubleL2.printList()
+#DoubleL2.printList()
+
+ith=3
+print('ith',ith)
+Anode = SimpleL1.select_ith(ith)
+if Anode: Anode.printself()
+else: print('node not found')
 
